4141381288c99c5b46b080160d0e277a494b0c4d
Sanyam <Sanyam@SANYAM>
2025-08-25 13:22:35 +0530

    update

---
+++ NationalPark_API_Project/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace NationalPark_API_Project
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
--- NationalPark_API_Project/Controllers/AccountController.cs
+++ NationalPark_API_Project/Controllers/AccountController.cs
@@ -15,14 +15,24 @@ namespace NationalPark_API_Project.Controllers
             _userRepository = userRepository;
         }
         [HttpPost("register")]
-        public IActionResult Register([FromBody] User user)                      //FromBody is used ---- DataSubmit HokrAye to
+        public IActionResult Register ([FromBody] User user)                      //FromBody is used ---- DataSubmit HokrAye to
         {
             if (ModelState.IsValid)
             {
-                var isUniqueUser =_userRepository.IsUniqueUser(user.UserName);  
-                if(!isUniqueUser) return BadRequest("User in Use!!!!");
+                var isUniqueUser = _userRepository.IsUniqueUser(user.UserName);
+                if (!isUniqueUser) return BadRequest("User in Use!!!!");
+                var UserInfo = _userRepository.Register(user.UserName, user.Password);
+                if (UserInfo == null) return NotFound();
+                user = UserInfo;
             }
+            return Ok(user);
         }
-
+        [HttpPost("login")] 
+        public IActionResult Login ([FromBody] User user)
+        {
+            var UserFromDb =_userRepository.Authenticate(user.UserName,user.Password);
+            if (UserFromDb == null) return BadRequest("Wrong User/PWD Enter");
+            return Ok(UserFromDb);
+        }
     }
 }
